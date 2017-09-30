1dc9850c4dfa1a4c61ee13f7da20b581aee06973
salva <salvadj1@hotmail.com>
2017-09-30 03:04:55 +0200

    Version 1.1

--- AntiBugOnSpawn.cs
+++ AntiBugOnSpawn.cs
@@ -30,7 +30,7 @@ namespace AntiBugOnSpawn
 
         public override Version Version
         {
-            get { return new Version("1.0"); }
+            get { return new Version("1.1"); }
         }
         private string red = "[color #B40404]";
         private string blue = "[color #81F7F3]";
@@ -41,18 +41,21 @@ namespace AntiBugOnSpawn
         private string white = "[color #FFFFFF]";
 
         public static IniParser cfg;
+        public static IniParser ClansIni;
+
         public static System.Random rnd;
         public bool RustPPSupport = false;
+        public bool ClansSupport = false;
 
         public override void Initialize()
         {
             Fougerite.Hooks.OnCommand += OnCommand;
             Fougerite.Hooks.OnPlayerSpawned += OnPlayerSpawned;
             Fougerite.Hooks.OnModulesLoaded += OnModulesLoaded;
-
             rnd = new System.Random();
             cfg = new IniParser(Path.Combine(ModuleFolder, "DefaultLoc.ini"));
         }
+
         public override void DeInitialize()
         {
             Fougerite.Hooks.OnCommand -= OnCommand;
@@ -62,39 +65,57 @@ namespace AntiBugOnSpawn
 
         public void OnModulesLoaded()
         {
+            ConsoleSystem.Print("");
             if (Fougerite.Server.GetServer().HasRustPP)
             {
                 RustPPSupport = true;
-                ConsoleSystem.Print("Plugin " + Name + " " + Version.ToString() + " Integration with Rust++ OK!!");
+                ConsoleSystem.Print( "Plugin " + Name + " " + Version.ToString() + " Integration with Rust++ OK!!");
+                if (File.Exists(Directory.GetCurrentDirectory() + "\\save\\PyPlugins\\Clans\\Clans.ini"))
+                {
+                    ClansSupport = true;
+                    ClansIni = new IniParser(Directory.GetCurrentDirectory() + "\\save\\PyPlugins\\Clans\\Clans.ini");
+                    ConsoleSystem.Print("Plugin " + Name + " " + Version.ToString() + " Integration with Clans OK!!");
+                }
+                else
+                {
+                    ConsoleSystem.PrintError("Plugin " + Name + " " + Version.ToString() + " Integration with Clans has Failed!! (OPTIONAL)");
+                    ClansSupport = false;
+                }
             }
             else
             {
-                ConsoleSystem.Print("Plugin " + Name + " " + Version.ToString() + " WARNING, integration with Rust++ has Failed!!");
+                ConsoleSystem.PrintError("WARNING ,Plugin " + Name + " " + Version.ToString() + " DISABLED, Not found Rust ++, make sure you have Rust ++ installed");
+                RustPPSupport = false;
+                Fougerite.Hooks.OnPlayerSpawned -= OnPlayerSpawned;
             }
+            ConsoleSystem.Print("");
         }
         public void OnCommand(Fougerite.Player player, string cmd, string[] args)
         {
             if (cmd == "antibug")
             {
-                player.MessageFrom("AntiBug", yellow + "ANTIBUG is a system that prevents players from using bugs in your house.");
-                player.MessageFrom("*", yellow + "To be able to SPAWN at a friend's house, he must be your friend.");
-                player.MessageFrom("*", yellow + "Your friend and you must use command " + blue + "/addfriend NAME");
+                player.MessageFrom("AntiBug", yellow + "/antibug is a system that prevents players from using bugs in your house.");
+                player.MessageFrom("*", yellow + " Option 1 - To be able to SPAWN at a friend's house, he must be your friend." + blue + "/addfriend NAME");
+                if (ClansSupport == true)
+                {
+                    player.MessageFrom("*", yellow + " Option 2 - To be able to SPAWN at a Clan's house, you need to be a member of a clan");
+                }
             }
         }
         public void OnPlayerSpawned(Fougerite.Player player, SpawnEvent se)
         {
-            foreach (Fougerite.Entity xx in Util.GetUtil().FindEntitiesAround(player.Location, 6.5f))
+            if (RustPPSupport == true)
             {
-                if (xx.Name.ToLower().Contains("wall") || xx.Name.ToLower().Contains("foundation") || xx.Name.ToLower().Contains("ceiling") || xx.Name.ToLower().Contains("pillar"))
+                foreach (Fougerite.Entity xx in Util.GetUtil().FindEntitiesAround(player.Location, 5f))
                 {
-                    if (xx.OwnerID == player.SteamID)
+                    if (xx.Name.ToLower().Contains("ceiling"))
                     {
-                        player.MessageFrom("AntiBug", green + "You spawned at you home :)");
-                        break;
-                    }
-                    else
-                    {
-                        if (RustPPSupport)
+                        if (xx.OwnerID == player.SteamID)
+                        {
+                            player.MessageFrom("AntiBug", green + "You spawned at: You Home :)");
+                            break;
+                        }
+                        else
                         {
                             var friendc = Fougerite.Server.GetServer().GetRustPPAPI().GetFriendsCommand.GetFriendsLists();
                             if (friendc.ContainsKey(xx.UOwnerID))
@@ -108,30 +129,115 @@ namespace AntiBugOnSpawn
                                 }
                                 else
                                 {
-                                    SendToSafeArea(player);
-                                    break;
+                                    if (ClansSupport == true)
+                                    {
+                                        RecuperaListaDeClanes();
+                                        if (SonDeElMismoClan(player.SteamID, xx.OwnerID, NombreDelClan(player.SteamID)))
+                                        {
+                                            player.MessageFrom("AntiBug", green + "You spawned at: Clan Home :)");
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            SendToSafeArea(player);
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        SendToSafeArea(player);
+                                        break;
+                                    }
                                 }
                             }
                             else
                             {
-                                SendToSafeArea(player);
-                                break;
+                                if (ClansSupport == true)
+                                {
+                                    RecuperaListaDeClanes();
+                                    if (SonDeElMismoClan(player.SteamID, xx.OwnerID, NombreDelClan(player.SteamID)))
+                                    {
+                                        player.MessageFrom("AntiBug", green + "You spawned at: Clan Home :)");
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        SendToSafeArea(player);
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    SendToSafeArea(player);
+                                    break;
+                                }
                             }
-                        }
-                        else
-                        {
-                            SendToSafeArea(player);
-                            break;
                         }
                     }
                 }
-            } 
+            }
+        }
+        public void RecuperaListaDeClanes()
+        {
+            ClansIni = new IniParser(Directory.GetCurrentDirectory() + "\\save\\PyPlugins\\Clans\\Clans.ini");
+            return;
+        }
+        public string NombreDelClan(string IDjugador)
+        {
+            string nombredelclan = "XXXXXXXXXXXXX";
+            if (ClansIni.ContainsSetting("ClanOwners", IDjugador))
+            { 
+                nombredelclan = ClansIni.GetSetting("ClanOwners", IDjugador);
+            }
+            else
+            {
+                if (ClansIni.ContainsSetting("ClanMembers", IDjugador))
+                {
+                    nombredelclan = ClansIni.GetSetting("ClanMembers", IDjugador);
+                }
+            }
+            return nombredelclan;
+        }
+        public bool SonDeElMismoClan(string IDjugador, string IDcasa, string ClanName)
+        {
+            if (ClansIni.ContainsSetting(ClanName, IDjugador) && ClansIni.ContainsSetting(ClanName, IDcasa))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool SonAmigosRustPP(string IDjugador, string IDcasa)
+        {
+            var friendc = Fougerite.Server.GetServer().GetRustPPAPI().GetFriendsCommand.GetFriendsLists();
+            if (friendc.ContainsKey(IDcasa))
+            {
+                var fs = (RustPP.Social.FriendList)friendc[IDcasa];
+                bool isfriend = fs.Cast<FriendList.Friend>().Any(friend => friend.GetUserID() == Convert.ToUInt64(IDjugador));
+                if (isfriend)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
         public void SendToSafeArea(Fougerite.Player player)
         {
-            player.MessageFrom("AntiBug", red + "Warning: " + yellow + "You have been moved to a random zone for security reasons");
-            player.MessageFrom("AntiBug", yellow + "You need to get" + blue + " /addfriend " + yellow + "of the owner of the house to be able to spawn on it");
-            player.MessageFrom("AntiBug", yellow + "More info " + red + " /antibug");
+            player.MessageFrom("AntiBug", red + "Warning: " + white + " /antibug " + yellow + " You have been moved to a random zone for security reasons");
+            player.MessageFrom("AntiBug", yellow + " Option 1 - You need to get" + blue + " /addfriend " + yellow + "of the OWNER of the house to be able to spawn on it");
+            if (ClansSupport == true)
+            {
+                player.MessageFrom("AntiBug", yellow + " Option 2 - You need to get" + blue + " Clan Menber " + yellow + "of the CLAN of the house to be able to spawn on it");
+            }
             int r = rnd.Next(1, 8156);
             string l = cfg.GetSetting("DefaultLoc", r.ToString());
             Vector3 v = Util.GetUtil().ConvertStringToVector3(l);
